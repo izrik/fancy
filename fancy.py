2d1270024309ca485e2f92949f21ffde2adf5f62
izrik <izrik@izrik.com>
2015-03-17 14:37:47 -0500

    Get notifications from github.

--- fancy.py
+++ fancy.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
+import requests
+import json
+
+with open('.token') as f:
+    tokens = f.read().splitlines()
 
 app = Flask(__name__)
 
-items = [
-    {'name': 'asdf', 'value': 1234},
-    {'name': 'qwer', 'value': 5678},
-    {'name': 'zxcv', 'value': 90},
-]
 
 @app.route('/')
 def index():
+
+    resp = requests.get('https://api.github.com/notifications',
+                        headers={'Authorization': 'token ' + tokens[0]})
+    items = json.loads(resp.text)
+
     return render_template('index.html', items=items)
 
 if __name__ == '__main__':
